360c3eb3eb164e92365548992cbe164b9c2894b6
craig dunlap <cadunlap3234@gmail.com>
2020-04-08 15:41:44 -0400

    added delete

--- ElevenNote.MVC/Controllers/NoteController.cs
+++ ElevenNote.MVC/Controllers/NoteController.cs
@@ -31,7 +31,7 @@ namespace ElevenNote.MVC.Controllers
         //add code here vvv
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(NoteCreate model)
+        public ActionResult NoteCreate(NoteCreate model)
         {
             if (!ModelState.IsValid) return View(model);
 
@@ -48,12 +48,66 @@ namespace ElevenNote.MVC.Controllers
             return View(model);
         }
 
+        public ActionResult NoteDetails(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
         private NoteService NewMethod()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
             return service;
         }
-
     }
 }
